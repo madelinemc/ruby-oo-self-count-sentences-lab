aaa196fd407f4b5b6d5868e02324ce1c09d794a2
madelinemc <madeline.coons@gmail.com>
2020-10-12 18:56:24 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array_of_split_strings = []
+    array_of_split_strings = self.delete(",").split(/[\.,?,!]/)
+    array_no_empty_strings = array_of_split_strings.reject { |string| string.empty? }
+    array_no_empty_strings.count
   end
 end
